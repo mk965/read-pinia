70895157bf2ac3a53476465f6ff782677be5820a
xiaoke <xiaoke@micous.com>
2023-04-14 15:11:15 +0800

    feat: 源码环境搭建

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { computed, ref } from 'vue';
+import { useStore } from '../store';
+
+const store = useStore();
+const count = computed(() => store.count);
+
+</script>
+
+<template>
+  <div class="card">
+    <button type="button" @click="store.increment">count is {{ count }}</button>
+  </div>
+</template>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
